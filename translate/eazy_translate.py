cb3dd62d25b8c11772048c392fb688f4d863b611
Douglas <126280321+d0uglas77p@users.noreply.github.com>
2023-08-25 00:48:15 -0300

    update

--- translate/eazy_translate.py
+++ translate/eazy_translate.py
@@ -27,33 +27,33 @@ while True:
         input("Precione qualquer tecla para voltar!")
         os.system("cls")
 
-    #Opcao 1: para a traducao de Ingles p Portugues
+    #Opcao 1: para a traducao de Ingles p/ Portugues
     os.system("cls")
     if opt=='1':
         translator = EasyGoogleTranslate(source_language='en', target_language='pt', timeout=10) #Responsavel pela traducao
 
-#Loop da opcao 1
+        #Loop da opcao 1
         while True:
             print("\n>>>>> Inglês/Português Br <<<<<")
-                #Mensgem do usuario
+            #Mensagem do usuario
             print("Mensagem: ",end='')
             result = translator.translate(input())
-    #Mensagem da traducao
+            #Mensagem da traducao
             print("Resultado:", result)
             opt2 = (input("\nPrecione qualquer tecla para continuar!\nDigite V para voltar ao menu! "))
             os.system("cls")
 
-            if opt2 in ['v']: #opcao de voltar ao menu
+            if opt2 in ['v']: #Opcao de voltar ao menu
                 opt2 == 'v'
                 break #Quebra do loop da opcao 1
 
-    # Opcao 2: para a traducao de Portuguesp Ingles
+    #Opcao 2: para a traducao de Portugues p/ Ingles
     elif opt=='2':
         translator = EasyGoogleTranslate(source_language='pt', target_language='en', timeout=10) #Responsavel pela traducao
 
         while True:
             print("\n>>>>> Português Br/Inglês <<<<<")
-            # Mensgem do usuario
+            #Mensgem do usuario
             print("Mensagem: ", end='')
             result = translator.translate(input())
             # Mensagem da traducao
@@ -61,11 +61,11 @@ while True:
             opt2 = (input("\nPrecione qualquer tecla para continuar!\nDigite V para voltar ao menu! "))
             os.system("cls")
 
-            if opt2 in 'v':#opcao de voltar ao menu
+            if opt2 in 'v': #Opcao de voltar ao menu
                 opt2 == 'v'
-                break#Quebra do loop da opcao 2
+                break   #Quebra do loop da opcao 2
 
-    # Opcao 3: obsercvao do programa exibida p usuario
+    #Opcao 3: observacao do programa exibida p usuario
     elif opt == '3':
         os.system("cls")
         print("Atenção: EAZY TRANSLATE está em fase de desenvolvimento.\n"
@@ -73,12 +73,12 @@ while True:
               "Por favor, digite a palavra correta!\n"
               "No momento contém apenas as traduções em Inglês e Português")
 
-##Opcao 4: sair do programa
+    #Opcao 4: sair do programa
     elif opt=='4':
         os.system("cls")
         print("Volte sempre ao EAZY TRANSLATE...!")
         sleep(delay)
-        break#Quebra do loop principal
+        break #Quebra do loop principal
 
 
 
