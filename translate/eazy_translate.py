3ec14e1a5bf2c4db7ff7bab2cc1c2f2441828387
Douglas <126280321+d0uglas77p@users.noreply.github.com>
2023-08-26 13:49:08 -0300

    organizacao-fixacao-comentarios

--- translate/eazy_translate.py
+++ translate/eazy_translate.py
@@ -5,6 +5,7 @@ import os
 from easygoogletranslate import EasyGoogleTranslate
 from tkinter import *
 
+#Funcao que limpar o que foi traduzido
 def limpar_texto():
     texto_traducao["text"] = ""
     texto_traducao2["text"] = ""
@@ -18,14 +19,15 @@ def show_menu():
 
 # Funcao para traduzir en/pt
 def translate_english_to_portuguese():
-    translator = EasyGoogleTranslate(source_language='en', target_language='pt', timeout=10)  # Responsável pela tradução
+    translator = EasyGoogleTranslate(source_language='en', target_language='pt', timeout=10)  # Responsável pela traducaoo
     texto_entrada_texto = texto_entrada.get()  # Obtenha o texto da entrada
     result = translator.translate(texto_entrada_texto)
 
     texto_traducao["text"] = result
 
+# Funcao para traduzir pt/en
 def translate_portuguese_to_english():
-    translator = EasyGoogleTranslate(source_language='pt', target_language='en', timeout=10)  # Responsável pela tradução
+    translator = EasyGoogleTranslate(source_language='pt', target_language='en', timeout=10)  # Responsável pela traducaoo
     texto_entrada_texto = texto_entrada2.get()  # Obtenha o texto da entrada
     result = translator.translate(texto_entrada_texto)
 
@@ -40,71 +42,55 @@ def show_observation():
           "Por favor, digite a palavra correta!\n"
           "No momento contém apenas as traduções em Inglês e Português")
 
-while True:
-    show_menu()
-    opt = input('Opção: ')
 
-    if opt not in ['1', '2', '3', '4']:
-        os.system("cls")
-        print("\nOpção Inválida")
-        input("Precione qualquer tecla para voltar!")
-        os.system("cls")
-
-    # Escolha de opcoes
-    elif opt == '1':
-        translate_english_to_portuguese()
-    elif opt == '2':
-        translate_portuguese_to_english()
-    elif opt == '3':
-        show_observation()
-    elif opt == '4':
-        os.system("cls")
-        print("Volte sempre ao EAZY TRANSLATE...!")
-        break  # Quebra do loop principal
-
-# Função de validação para limitar o comprimento da entrada
+# Função de validação para limitar os caracteres na caixa de entrada
 def validate_input_length(P):
-    max_length = 120  # Defina o comprimento máximo desejado
+    max_length = 99  # definicao máximo desejado
     if len(P) > max_length:
         return False
     return True
 
-# FRONT
+### MAIN FRONT
 janela = Tk()
 janela.title("Eazy Translate")
-janela.geometry("760x400")
+janela.geometry("750x400")
 janela.resizable(False, False)  # Impede a redimensionamento horizontal e vertical
 
 
 #texto_titulo = Label(janela, text="Eazy")
 #texto_titulo.grid(column=7, row=1, padx=200, pady=10)
 
-
+# Botao para traducao de En/Pt
 botao_enpt = Button(janela, text="Traduzir: Inglês / Português", command=translate_english_to_portuguese )
 botao_enpt.grid(column=0, row=1, padx=10, pady=20)
 
-texto_entrada = Entry(janela, width=120, validate="key") #120 caracteres
+# Caixa de entrada do usuario En/Pt
+texto_entrada = Entry(janela, width=100, validate="key") #99 caracteres
 texto_entrada.grid(column=0, row=2, padx=10, pady=10)
 
-texto_traducao = Label(janela, text="")
+# Texto traduzido da caixa de entrada En/Pt
+texto_traducao = Label(janela, width=100, text="")
 texto_traducao.grid(column=0, row=3, padx=10, pady=10)
 
+# Botao para traducao de Pt/En
 botao_pten = Button(janela, text="Traduzir: Português / Inglês", command=translate_portuguese_to_english )
 botao_pten.grid(column=0, row=4, padx=10, pady=20)
 
-texto_entrada2 = Entry(janela, width=120, validate="key") #120 caracteres
+# Caixa de entrada do usuario Pt/En
+texto_entrada2 = Entry(janela, width=100, validate="key") #99 caracteres
 texto_entrada2.grid(column=0, row=5, padx=10, pady=10)
 
-texto_traducao2 = Label(janela, text="")
+# Texto traduzido da caixa de entrada Pt/En
+texto_traducao2 = Label(janela, width=100, text="")
 texto_traducao2.grid(column=0, row=6, padx=10, pady=10)
 
+# Botao para limpar
 botao_clear = Button(janela, text="Limpar", command=lambda: [limpar_texto(), texto_entrada2.delete(0, 'end'), texto_entrada.delete(0, 'end')])
 botao_clear.grid(column=0, row=7, padx=10, pady=10)
 
-# Associacao a função de validação à caixa de entrada
+# Associacao a função de validação à caixa de entrada/ limites de caracteres da caixa de entrada
 validate_input_length_cmd = janela.register(validate_input_length)
 texto_entrada.config(validatecommand=(validate_input_length_cmd, '%P'))
 texto_entrada2.config(validatecommand=(validate_input_length_cmd, '%P'))
 
 janela.mainloop()
-
