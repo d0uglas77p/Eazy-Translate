21ff2e5519830f758550f9f05f304b4bbe2bc787
Douglas <126280321+d0uglas77p@users.noreply.github.com>
2023-08-26 18:01:06 -0300

    Versao 1.0

--- translate/eazy_translate.py
+++ translate/eazy_translate.py
@@ -1,20 +1,26 @@
-### EAZY TRANSLATE pt/en
+### EAZY TRANSLATE
 ### Douglas P. Ferreira
 
-import os
+#Programa que traduz o inglês para o português e vice-versa.
+#Utilizando apenas o Python e bibliotecas.
+#EasyGoogleTranslate para a tradução
+#Tkinter para o configurar o ambiente gráfico.
+#version 1.0
+
+# INICIO
+# Bibliotecas
 from easygoogletranslate import EasyGoogleTranslate
 from tkinter import *
 import tkinter as tk
 
-#Funcao que limpar o que foi traduzido
+# Funcao que limpa o que foi traduzido
 def limpar_texto():
     texto_traducao["text"] = ""
     texto_traducao2["text"] = ""
     show_menu()
 
-# Funcao de opcao do menu
+## BACK / Funcao de opcao do menu
 def show_menu():
-    os.system("cls")
     print("\nEscolha a opção desejada!")
     print("[1] Inglês/Português Br\n[2] Português Br/Inglês\n[3] Observação\n[4] Sair")
 
@@ -34,15 +40,6 @@ def translate_portuguese_to_english():
 
     texto_traducao2["text"] = result
 
-# Funcao de observacao do programa
-def show_observation():
-    os.system("cls")
-    print("Atenção: EAZY TRANSLATE está em fase de desenvolvimento.\n"
-          "Qualquer palavra que for digitado errado ele poderá entender de outra forma!\n"
-          "Por favor, digite a palavra correta!\n"
-          "No momento contém apenas as traduções em Inglês e Português")
-
-
 # Função de validação para limitar os caracteres na caixa de entrada
 def validate_input_length(P):
     max_length = 99  # definicao máximo desejado
@@ -53,24 +50,39 @@ def validate_input_length(P):
 ### MAIN FRONT
 janela = tk.Tk()
 janela.title("Eazy Translate v.1")
-janela.geometry("750x450")
+janela.geometry("750x470")
 janela.resizable(False, False)  # Impede a redimensionamento horizontal e vertical
 janela.configure(bg="#1c1c1c")
-janela.iconbitmap('C:\\Users\\dougl\\Desenvolvimento\\dev\\icon.ico')
+janela.iconbitmap('C:\\Users\\dougl\\Desenvolvimento\\python\\GitHub\\Eazy-Translate\\translate\\icon.ico')
+
+# Funcao que abre uma nova janela de observacao do programa
+def new_janela():
+    observation_janela = tk.Toplevel(janela)
+    observation_janela.title("Observação")
+    observation_janela.geometry("400x199")
+    observation_janela.resizable(False, False)  # Impede a redimensionamento horizontal e vertical
+    observation_janela.configure(bg="#1c1c1c")
+    observation_janela.iconbitmap('C:\\Users\\dougl\\Desenvolvimento\\python\\GitHub\\Eazy-Translate\\translate\\icon.ico')
+
+    texto_titulo = Label(observation_janela, width=55, height=12, borderwidth=2, text="EAZY TRANSLATE está em fase de desenvolvimento. versão 1.0"
+                                                                                      "\nPor favor, digite a palava correta para evitar erros!"
+                                                                                      "\nNo momento contém apenas as traduções em inglês e português."
+                                                                                      "\nEm breve terá outras linguagens."
+                                                                                      "\nSe encontrar bugs e afins, dar feedback, pode entrar em contado:"
+                                                                                      "\ndouglas_hotmail.com@hotmail.com"
+                                                                                      "\nObrigado por testar! :D by Douglas P. Ferreira")
+    texto_titulo.grid(column=0, row=1, padx=5, pady=5)
+    texto_titulo.configure(bg="#1c1c1c", fg="white")
 
 # Objeto invisivel para fins de organizacao
 inv = Label()
 inv.grid(column=0, row=1, padx=10, pady=10)
 inv.configure(bg="#1c1c1c")
 
-#texto_titulo = Label(janela, text="Eazy Translate - Demo", width=30, height=3, borderwidth=2)
-#texto_titulo.grid(column=0, row=1, padx=5, pady=5)
-#texto_titulo.configure(bg="#00adfc")
-
 # Botao para traducao de En/Pt
-botao_enpt = Button(janela, text="Traduzir: Inglês / Português", command=translate_english_to_portuguese )
+botao_enpt = Button(janela, text="Traduzir: Inglês / Português", borderwidth=4, command=translate_english_to_portuguese )
 botao_enpt.grid(column=0, row=2, padx=10, pady=20)
-botao_enpt.configure(bg="#00ff6e")
+botao_enpt.configure(bg="#1ad904")
 
 # Caixa de entrada do usuario En/Pt
 texto_entrada = Entry(janela, width=100, validate="key") #99 caracteres
@@ -81,9 +93,9 @@ texto_traducao = Label(janela, width=100, text="", borderwidth=10)
 texto_traducao.grid(column=0, row=4, padx=10, pady=10)
 
 # Botao para traducao de Pt/En
-botao_pten = Button(janela, text="Traduzir: Português / Inglês", command=translate_portuguese_to_english )
+botao_pten = Button(janela, text="Traduzir: Português / Inglês", borderwidth=4, command=translate_portuguese_to_english )
 botao_pten.grid(column=0, row=5, padx=10, pady=20)
-botao_pten.configure(bg="#00ff6e")
+botao_pten.configure(bg="#1ad904")
 
 # Caixa de entrada do usuario Pt/En
 texto_entrada2 = Entry(janela, width=100, validate="key") #99 caracteres
@@ -96,7 +108,11 @@ texto_traducao2.grid(column=0, row=7, padx=10, pady=10)
 # Botao para limpar
 botao_clear = Button(janela, text="Limpar", command=lambda: [limpar_texto(), texto_entrada2.delete(0, 'end'), texto_entrada.delete(0, 'end')])
 botao_clear.grid(column=0, row=8, padx=10, pady=10)
-botao_clear.configure(bg="#f58727")
+botao_clear.configure(bg="#ff6a00")
+
+# Botao que abre as observacoes
+botao_leia = Button(janela, text="Leia", borderwidth=3, command=new_janela)
+botao_leia.place(x=10, y=430)
 
 # Associacao a função de validação à caixa de entrada/ limites de caracteres da caixa de entrada
 validate_input_length_cmd = janela.register(validate_input_length)
@@ -104,3 +120,4 @@ texto_entrada.config(validatecommand=(validate_input_length_cmd, '%P'))
 texto_entrada2.config(validatecommand=(validate_input_length_cmd, '%P'))
 
 janela.mainloop()
+# FIM
